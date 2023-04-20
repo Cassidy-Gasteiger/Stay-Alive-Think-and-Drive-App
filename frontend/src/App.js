4b3642eb69294c545f1fe7ac8eae8f7122bbd1e9
Christopher Simpkins <chris.simpkins@gmail.com>
2023-04-20 18:25:22 -0400

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// rfce - to fill boilerplate code for a new component
+// npm start - use this command in terminal to view the front end in browser
+
+
+import React from 'react'
+import Map from './components/Map'
+
+function App() {
+  return (
+    <div>
+    <div className='bg-red-400'>Stay Alive: Think and Drive</div>
+    <Map />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <div className='h-20 bg-green-400'>Map</div>
+  )
+}
+
+export default Map
