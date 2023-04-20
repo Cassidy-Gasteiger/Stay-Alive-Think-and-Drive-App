4497ce40ffe24192c6f1a725071eae9afe995bee
Ashish Dhiman <ashish1610dhiman@gmail.com>
2023-04-20 17:18:39 -0400

    chose bad weather from data

--- utilities.py
+++ utilities.py
@@ -8,6 +8,7 @@ import numpy as np
 import datetime
 from meteostat import Stations,Daily,Hourly
 import pickle
+from scipy import stats
 
 def haversine_distance(lat1, lon1, lat2, lon2):
     """
@@ -27,7 +28,7 @@ def closest_airport(lat,long):
     :param long: longitude current location
     :return: closest weather station
     """
-    latitude_longitude_repo = pd.read_csv("./station_lat_longitude.csv", index_col=0)
+    latitude_longitude_repo = pd.read_csv("../data/station_lat_longitude.csv", index_col=0)
     latitude_longitude_repo["distance"] = latitude_longitude_repo.apply(
         lambda x: haversine_distance(lat1=lat, lon1=long, \
                                      lat2=x["latitude"], lon2=x["longitude"]), axis=1)
@@ -71,7 +72,7 @@ def transform_weather(weather_avg_6):
     pres_in = weather_avg_6[5] * 0.02952998057228486
     return np.array([temp_f, hum, pres_in, precip_in, wspd_mph])
 
-def weather_odds_helper(kde_pca_accidents,kde_pca_history,weather_now,N=100):
+def weather_odds_helper(kde_pca_accidents,kde_pca_history,weather_now,if_bad=False,N=100):
     """
     :param kde_pca_accidents: KDE PCA dict for airport from accidents data
     :param kde_pca_history: KDE PCA dict for airport from history data
@@ -83,7 +84,9 @@ def weather_odds_helper(kde_pca_accidents,kde_pca_history,weather_now,N=100):
     time_ad = get_discreet_time()
     #get weather avg from history
     kde_kernel_accidents = kde_pca_accidents["kde"][time_ad]
+    kde_kernel_accidents = stats.gaussian_kde(kde_kernel_accidents.dataset)
     kde_kernel_history = kde_pca_history["KDE"][time_ad]
+    kde_kernel_history = stats.gaussian_kde(kde_kernel_history.dataset)
     weather_avg_samples = np.array([kde_kernel_history.resample(160).mean(axis=1) for i in range(N)])
     weather_avg_hist = np.mean(weather_avg_samples,axis=0)
     #densities from hist data
@@ -99,6 +102,8 @@ def weather_odds_helper(kde_pca_accidents,kde_pca_history,weather_now,N=100):
     weather_avg_acc_pca = kde_pca_accidents["pca"].transform(weather_avg_acc.reshape(1,-1))
     density_weather_avg_acc = kde_kernel_accidents.evaluate(weather_avg_acc_pca)[0]
     density_weather_now_acc = kde_kernel_accidents.evaluate(weather_now_acc_pca)[0]
+    if if_bad:
+        density_weather_now_acc = density_weather_now_acc*1.1
     print (f""" density_weather_now_acc:{density_weather_now_acc:.6f}
     density_weather_avg_acc:{density_weather_avg_acc:.6f}
     density_weather_avg_hist:{density_weather_avg_hist:.6f}
@@ -123,21 +128,29 @@ def get_odds(lat,long, N= 250):
     print ("Fetching current weather")
     weather_now = get_realtime_weather(airport_api_id)
     # read kde estimate for historical data
-    with open('accidents_kde.pickle', 'rb') as f:
+    with open('../data/accidents_kde.pickle', 'rb') as f:
         # Load the data from the pickle file
         kde_accidents_dict = pickle.load(f)
-    with open('kde_dict.pickle', 'rb') as f:
+    with open('../data/kde_dict.pickle', 'rb') as f:
         # Load the data from the pickle file
         kde_historical_dict = pickle.load(f)
     kde_pca_accidents = kde_accidents_dict[airport_icao]
     kde_pca_history = kde_historical_dict[airport_icao]
     print ("Densities for weather now")
-    weather_odds_normal = weather_odds_helper(kde_pca_accidents, kde_pca_history, weather_now,N)
+    weather_odds_normal = weather_odds_helper(kde_pca_accidents, kde_pca_history, weather_now,False,N)
     weather_now_bad = weather_now.copy()
-    weather_now_bad["prcp"] = 10
-    weather_now_bad["temp"] = 0
-    weather_now_bad["pres"] = 980
+    kde_pca_accidents = kde_accidents_dict["KBMG"]
+    weather_bad_acc_samples = kde_pca_accidents["kde"][get_discreet_time()].resample(250)
+    kde2 = stats.gaussian_kde(kde_pca_accidents["kde"][get_discreet_time()].dataset)
+    weather_bad_acc_samples_density = kde2.evaluate(weather_bad_acc_samples)
+    # print(weather_bad_acc_samples.shape,weather_bad_acc_samples_density.shape)
+    weather_bad = weather_bad_acc_samples[:,np.argmax(weather_bad_acc_samples_density)]
+    weather_bad_5cols = kde_pca_accidents["pca"].inverse_transform(weather_bad)
+    weather_now_bad["prcp"] = weather_bad_5cols[3] * 25.4
+    weather_now_bad["temp"] = (weather_bad_5cols[0]-32)/1.8
+    weather_now_bad["pres"] = weather_bad_5cols[2] / 0.02952998057228486
+    weather_now_bad["wspd"] = weather_bad_5cols[4] / 0.02952998057228486
     print("Densities for weather bad")
-    weather_odds_bad = weather_odds_helper(kde_pca_accidents, kde_pca_history, weather_now_bad, N)
+    weather_odds_bad = weather_odds_helper(kde_pca_accidents, kde_pca_history, weather_now_bad,True, N)
     print(f"\nOdds for weather_now={weather_odds_normal:.5f} vs Odds for weather_bad={weather_odds_bad:.5f}")
     return (weather_odds_normal,weather_odds_bad)
